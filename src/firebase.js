31843192a4d358134b3a9fa877e6e3046eaf68ab
David Schurer <david.schurer@yahoo.com>
2025-04-29 01:57:25 -0700

    hiding those firebase api keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,17 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth"; // Import Firebase Authentication
+require('dotenv').config();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_yxPOVMqh2thXE_RjpQxgM49MJc38rso",
-  authDomain: "visualsketchsync.firebaseapp.com",
-  projectId: "visualsketchsync",
-  storageBucket: "visualsketchsync.firebasestorage.app",
-  messagingSenderId: "1092098323148",
-  appId: "1:1092098323148:web:2fcea07f7dba11cdeb0869",
-  measurementId: "G-R19CC0VEVK",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
